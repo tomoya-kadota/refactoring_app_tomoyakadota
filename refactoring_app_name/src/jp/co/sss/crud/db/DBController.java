bd245e0711d0eece3695aae17efd9af77837bd5a
ユーザー <@DESKTOP-F98KV7P>
2026-01-09 16:20:47 +0900

    fix:角田　DBController.javaの変数名及びメソッド名を変更
    【対象ファイル】
    DBController.java
    
    【メソッド名の変更】
    ①find() -> findAll()
    
    ②findB() -> findByEmpName()
    
    ③findC(String deptId) -> findByDeptId(String deptId)
    
    【変数名の変更】
    ①232行目：int deptId2 -> int resultDeptId
    ※上記変更に伴い、233、235行目で使用されている同一変数名の変更処理を実施済
    
    【誤植の修正】
    ①237行目：} else if (gender == 3) { -> } else if (resultDeptId == 3) {
    →性別が3の時に総務部と出力されてしまうため、修正を加えております

--- refactoring_app_name/src/jp/co/sss/crud/db/DBController.java
+++ refactoring_app_name/src/jp/co/sss/crud/db/DBController.java
@@ -30,7 +30,7 @@ public class DBController {
 	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
 	 * @throws SQLException           DB処理でエラーが発生した場合に送出
 	 */
-	public static void find() throws ClassNotFoundException, SQLException {
+	public static void findAll() throws ClassNotFoundException, SQLException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
@@ -45,7 +45,7 @@ public class DBController {
 			// SQL文を実行
 			resultSet = preparedStatement.executeQuery();
 
-			//resultSetの結果Setがない場合はfalse
+			// resultSetの結果Setがない場合はfalse
 			if (!resultSet.isBeforeFirst()) {
 				System.out.println("該当者はいませんでした");
 				return;
@@ -93,7 +93,7 @@ public class DBController {
 	 * @throws SQLException           DB処理でエラーが発生した場合に送出
 	 * @throws IOException            入力処理でエラーが発生した場合に送出
 	 */
-	public static void findB() throws ClassNotFoundException, SQLException, IOException {
+	public static void findByEmpName() throws ClassNotFoundException, SQLException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		// 検索ワード
@@ -173,7 +173,7 @@ public class DBController {
 	 * @throws SQLException           DB処理でエラーが発生した場合に送出
 	 * @throws IOException            入力処理でエラーが発生した場合に送出
 	 */
-	public static void findC(String deptId) throws ClassNotFoundException, SQLException, IOException {
+	public static void findByDeptId(String deptId) throws ClassNotFoundException, SQLException, IOException {
 
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
@@ -229,12 +229,12 @@ public class DBController {
 				System.out.print("\t");
 
 				String deptIdString = resultSet.getString("dept_id");
-				int deptId2 = Integer.parseInt(deptIdString);
-				if (deptId2 == 1) {
+				int resultDeptId = Integer.parseInt(deptIdString);
+				if (resultDeptId == 1) {
 					System.out.println("営業部");
-				} else if (deptId2 == 2) {
+				} else if (resultDeptId == 2) {
 					System.out.println("経理部");
-				} else if (gender == 3) {
+				} else if (resultDeptId == 3) {
 					System.out.println("総務部");
 
 				}
@@ -254,14 +254,14 @@ public class DBController {
 	/**
 	 * 社員情報を1件登録
 	 * 
-	 * @param empName 社員名
-	 * @param gender 性別
+	 * @param empName  社員名
+	 * @param gender   性別
 	 * @param birthday 生年月日
-	 * @param deptId 部署ID
+	 * @param deptId   部署ID
 	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
-	 * @throws SQLException            DB処理でエラーが発生した場合に送出
-	 * @throws IOException             入力処理でエラーが発生した場合に送出
-	 * @throws ParseException 
+	 * @throws SQLException           DB処理でエラーが発生した場合に送出
+	 * @throws IOException            入力処理でエラーが発生した場合に送出
+	 * @throws ParseException
 	 */
 	public static void insert(String empName, String gender, String birthday, String deptId)
 			throws ClassNotFoundException, SQLException, IOException, ParseException {
@@ -297,12 +297,11 @@ public class DBController {
 	 * 
 	 * @param empId 社員ID
 	 * @throws ClassNotFoundException ドライバクラスが不在の場合に送出
-	 * @throws SQLException            DB処理でエラーが発生した場合に送出
-	 * @throws IOException             入力処理でエラーが発生した場合に送出
-	 * @throws ParseException 
+	 * @throws SQLException           DB処理でエラーが発生した場合に送出
+	 * @throws IOException            入力処理でエラーが発生した場合に送出
+	 * @throws ParseException
 	 */
-	public static void update(String empId)
-			throws ClassNotFoundException, SQLException, IOException, ParseException {
+	public static void update(String empId) throws ClassNotFoundException, SQLException, IOException, ParseException {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
