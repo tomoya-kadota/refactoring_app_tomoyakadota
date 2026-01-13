5406eacafa3d1369e627f35b3c0081abc77a9774
ユーザー <@DESKTOP-F98KV7P>
2026-01-13 13:16:55 +0900

    add:角田　下記2ファイルについて不可視フィールド、及びgetter、setterの追加
    ・DTOクラス
    Department.java
    Employee.java

--- refactoring_app_name/src/jp/co/sss/crud/dto/Department.java
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Department.java
@@ -2,4 +2,22 @@ package jp.co.sss.crud.dto;
 
 public class Department {
 
+	private Integer deptId;
+	private String deptName;
+
+	public Integer getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
 }
--- refactoring_app_name/src/jp/co/sss/crud/dto/Employee.java
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Employee.java
@@ -1,5 +1,53 @@
 package jp.co.sss.crud.dto;
 
+import java.util.Date;
+
 public class Employee {
+
+	private Integer empId;
+	private String empName;
+	private Integer gender;
+	private Date birthday;
+	private Department department;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 
 }
